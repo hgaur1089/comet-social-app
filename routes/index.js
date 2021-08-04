603c09ed6d9883eae87c67f284443c6747059966
hgaur1089 <60226963+hgaur1089@users.noreply.github.com>
2021-08-04 20:13:18 +0530

    created controller and routes for posts and users and installed ejs

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,10 @@ const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
 router.get('/', homeController.home);
+router.use('/users', require('./users'));
+router.use('/posts', require('./posts'));
+
+//for any further routes, access from here
+//router.use('/routerName, require('./requireFile'));
 
 module.exports = router;
