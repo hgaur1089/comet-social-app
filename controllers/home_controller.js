6f407197609508741441a6bea40cad93c0f43218
hgaur1089 <60226963+hgaur1089@users.noreply.github.com>
2021-08-17 15:51:21 +0530

    created and displayed posts on home screen by populating the user(i. e. showing username)

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,20 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req, res){
-    return res.render('home.ejs', {
-        title: "Home"
-    }); 
+
+    // Post.find({}, function(err, posts){
+    //     return res.render('home.ejs', {
+    //         title: "Comet | Home",
+    //         posts: posts
+    //     }); 
+    // })
+
+    //populate the user of each post
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home.ejs', {
+            title: "Comet | Home",
+            posts: posts
+        });
+    });
+
 }
